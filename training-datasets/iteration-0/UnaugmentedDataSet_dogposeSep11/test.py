95e01beffd28c59cb0c523287503f9ed64516311
Mystic-SuperRich <totocaca@naver.com>
2021-09-11 14:56:26 +0900

    .

---
+++ training-datasets/iteration-0/UnaugmentedDataSet_dogposeSep11/test.py
@@ -0,0 +1,4 @@
+from scipy import io 
+mat_file = io.loadmat('C:/Users/Master/Desktop/dic/DeepLabCut/conda-environments/dogpose-dogpo-2021-09-11/training-datasets/iteration-0/UnaugmentedDataSet_dogposeSep11/dogpose_dogpo95shuffle1.mat')
+
+print(mat_file)
